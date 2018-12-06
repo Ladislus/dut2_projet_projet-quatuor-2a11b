acf323e3f1a1a81aaef4f7da5307cc42d4d5456c
Ladislas WALCAK <walcak.ladislas@gmail.com>
2018-12-06 11:58:35 +0100

    Correction du Ressources/setup.sh

--- Ressources/setup.sh
+++ Ressources/setup.sh
@@ -1,4 +1,8 @@
-virtualenv -p python3 ../Developpement/venv
+if [ ! -d ../Developpement/venv ]; then
+  virtualenv -p python3 ../Developpement/venv
+fi
+
+source ../Developpement/venv/bin/activate
 pip install -r requirement.txt
 
 echo 'FLASK_APP =' > ../Developpement/.flaskenv
