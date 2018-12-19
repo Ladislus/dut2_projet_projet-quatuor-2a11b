35f5af0de03866547ee4feffceae7a3a3fd96411
Louis Bonduel <bonduel@info27-11.iut45.univ-orleans.fr>
2018-12-19 11:54:37 +0100

    Ajout des routes administration -- pas fini

--- Developpement/views_other.py
+++ Developpement/views_other.py
@@ -10,6 +10,14 @@ def contact_liens():
 def contact_connexion():
     """
 
-    :return: Retourne le template de la page dédiée aux liens utiles
+    :return: Retourne le template de la page de connexion
     """
     return render_template("contact_connexion.html")
+
+@app.route("/other/mdpOublie")
+def contact_mdpOublie():
+    """
+
+    :return: Retourne le template de la page d'oublie de mot de passe
+    """
+    return render_template("contact_mdpOublie.html")
