9255a62cea448ba681571c375594ac11b7a86cd6
Michael <micha_45@hotmail.fr>
2018-12-23 22:06:49 +0100

    premiere version de la page quatuor_presse.html (Version Semi-Brute)

--- Developpement/views_quatuor.py
+++ Developpement/views_quatuor.py
@@ -52,9 +52,9 @@ def quatuor_presse_actu():
 
 
 liste=[]
-liste.append(["Le Quatuor a un nouveau site Web","22/12/2018","Olivier Roussillat"])
-liste.append(["Le Groupe 2A11b a fait le meilleur site web","23/12/2018","Sophie Anglade"])
-liste.append(["Car c'est notre projet!","24/12/2018","Andrien Foucault"])
+liste.append(["Le Quatuor a un nouveau site Web","22/12/2018","Olivier Roussillat","presentation-quatuor-1","Omitto iuris dictionem in libera civitate contra leges senatusque consulta; caedes relinquo; libidines praetereo, quarum acerbissimum extat indicium et ad insignem memoriam turpitudinis et paene ad iustum odium imperii nostri, quod constat nobilissimas virgines se in puteos abiecisse et morte voluntaria necessariam turpitudinem depulisse. Nec haec idcirco omitto, quod non gravissima sint, sed quia nunc sine teste dico."])
+liste.append(["Le Groupe 2A11b a fait le meilleur site web","23/12/2018","Sophie Anglade","presentation-quatuor-2","Omitto iuris dictionem in libera civitate contra leges senatusque consulta; caedes relinquo; libidines praetereo, quarum acerbissimum extat indicium et ad insignem memoriam turpitudinis et paene ad iustum odium imperii nostri, quod constat nobilissimas virgines se in puteos abiecisse et morte voluntaria necessariam turpitudinem depulisse. Nec haec idcirco omitto, quod non gravissima sint, sed quia nunc sine teste dico."])
+liste.append(["Car c'est notre projet!","24/12/2018","Andrien Foucault","presentation-quatuor-3","Omitto iuris dictionem in libera civitate contra leges senatusque consulta; caedes relinquo; libidines praetereo, quarum acerbissimum extat indicium et ad insignem memoriam turpitudinis et paene ad iustum odium imperii nostri, quod constat nobilissimas virgines se in puteos abiecisse et morte voluntaria necessariam turpitudinem depulisse. Nec haec idcirco omitto, quod non gravissima sint, sed quia nunc sine teste dico."])
 
 @app.route("/quatuor/presse/all")
 def quatuor_presse_all():
@@ -67,8 +67,13 @@ def quatuor_presse_all():
 
 @app.route("/quatuor/presse/article/<nomarticle>")
 def quatuor_presse(nomarticle):
+    article=None
+    for elem in liste:
+        if elem[0]==nomarticle:
+            article=elem
+
     """
     :id: L'id de l'article de presse
     :return: Retourne le template correspondant a la page d'un artciel du quautor
     """
-    return render_template("quatuor_presse.html",article=nomarticle)
+    return render_template("quatuor_presse.html",article=article)
