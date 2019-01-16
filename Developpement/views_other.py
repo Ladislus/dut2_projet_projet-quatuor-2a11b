c44104b7241e95b30b757296aace56247ba3ea24
Ladislas WALCAK <walcak.ladislas@gmail.com>
2019-01-16 15:06:44 +0100

    Correction conflits de merge avec Michael

--- Developpement/views_other.py
+++ Developpement/views_other.py
@@ -5,7 +5,7 @@ def contact_liens():
 
     :return: Retourne le template de la page dédiée aux liens utiles
     """
-    return render_template("contact_liens.html")
+    return render_template("other/page_liens.html")
 
 @app.route("/other/connexion/")
 def contact_connexion():
@@ -13,7 +13,7 @@ def contact_connexion():
 
     :return: Retourne le template de la page de connexion
     """
-    return render_template("contact_connexion.html")
+    return render_template("other/connexion.html")
 
 @app.route("/other/mdpOublie/")
 def contact_mdpOublie():
