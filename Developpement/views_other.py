98eef0a512b910b68f88e2b42eaeae315c2941bc
Michael Abid <abid@info23-14.iut45.univ-orleans.fr>
2019-01-16 16:44:32 +0100

    Merge remote-tracking branch 'main/master'
    fusion est
    dans une branche de sujet.

--- Developpement/views_other.py
+++ Developpement/views_other.py
@@ -1,22 +1,23 @@
 from .views import *
 @app.route("/other/liens/")
-def contact_liens():
+def other_liens():
     """
 
     :return: Retourne le template de la page dédiée aux liens utiles
     """
     return render_template("other/page_liens.html")
 
-@app.route("/other/connexion/")
-def contact_connexion():
+@app.route("/other/connection/")
+def other_connexion():
     """
 
-    :return: Retourne le template de la page de connexion
+    :return: Retourne le templates de la page de connexion
+
     """
-    return render_template("other/connexion.html")
+    return render_template("base.html")
 
 @app.route("/other/deconnexion/")
-def contact_deconnexion():
+def other_deconnexion():
     """
 
     :return: Retourne le template de la page de deconnexion
@@ -24,7 +25,7 @@ def contact_deconnexion():
     return render_template("other/deconnexion.html")
 
 @app.route("/other/mdpOublie/")
-def contact_mdpOublie():
+def other_mdpOublie():
     """
 
     :return: Retourne le template de la page d'oublie de mot de passe
@@ -32,7 +33,7 @@ def contact_mdpOublie():
     return render_template("other/mdpOublie.html")
 #NE PAS PRENDRE EN COMPTE
 @app.route("/other/test/")
-def contact_test():
+def other_test():
     """
 
     :return: Retourne le template de la page de test
