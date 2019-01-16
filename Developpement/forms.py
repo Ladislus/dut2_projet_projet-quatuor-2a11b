2d198f75d8cf143499bb0418a79d59aa4cccf466
Adrien Foucault <foucault@info23-15.iut45.univ-orleans.fr>
2019-01-16 11:24:00 +0100

    [ADD] fichier forms.py + forms de person, user et stage

---
+++ Developpement/forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, HiddenField, PasswordField, DateField, IntegerField, FloatField
+from wtforms.validators import DataRequired
+
+class PersonForm(FlaskForm):
+    id          = HiddenField('idPers')
+    nomPers     = StringField('Nom', validators[DataRequired()])
+    prenomPers  = StringField('Prénom', validators[DataRequired()])
+    mailPers    = StringField('Adresse Mail', validators[DataRequired()])
+    tel1Pers    = StringField('Téléphone principal', validators[DataRequired()])
+    tel2Pers    = StringField('Téléphone optionnel')
+    dateNPers   = DateField('Date de naissance', validators[DataRequired()])
+    idTuteur    = HiddenField('idTuteur')
+
+    idLieu      = StringField('Adresse', validators[DataRequired()])
+
+class UserForm(FlaskForm):
+    idUt          = HiddenField('idUt')
+    ecoleUt     = StringField('Ecole de Musique')
+    nivUt       = IntegerField('Niveau')
+    usernameUt  = StringField('Nom d\'utlisateur', validators[DataRequired()])
+    mdpUt       = PasswordField('Mot de Passe', validators[DataRequired()])
+
+    idPers      = HiddenField('personneLiee ?')
+
+class StageForm(FlaskForm):
+    idSt        = HiddenField('idSt')
+    idRep       = HiddenField('idRep')
+    intituleSt  = StringField('Intitule du stage', validators[DataRequired()])
+    nbPlaceSt   = IntegerField('Nombre de places dispos', validators[DataRequired()])
+    dateDebSt   = DateField('Date de début du stage', validators[DataRequired()])
+    dateFinSt   = DateField('Date de fin du stage', validators[DataRequired()])
+    tenueSt     = StringField('Tenue exigée pour le concert du stage')
+    prixSt      = FloatField('Prix du stage')
+    descStage   = TextField('Description du stage', validators[DataRequired()])
+    nivRequisSt = IntegerField('Niveau minimum pour intégrer le stage')
+
+    idLieu      = StringField('Adresse', validators[DataRequired()]) 
