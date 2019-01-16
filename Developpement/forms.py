e6f08af01be0d80117f8d6263b5051bb69b970c1
Adrien Foucault <foucault@info23-15.iut45.univ-orleans.fr>
2019-01-16 15:15:21 +0100

    [MOD] forms pour le site

--- Developpement/forms.py
+++ Developpement/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, HiddenField, PasswordField, DateField, IntegerField, FloatField, FieldList, FormField
+from wtforms import StringField, DateField, FieldList, FormField, IntegerField, TextField, FileField, PasswordField, HiddenField
 from wtforms.validators import DataRequired
 
 class PersonForm(FlaskForm):
@@ -7,23 +7,43 @@ class PersonForm(FlaskForm):
     prenomPers      = StringField('Prénom', validators[DataRequired()])
     dateNPers       = DateField('Date de naissance', validators[DataRequired()])
     tel1Pers        = StringField('Téléphone principal', validators[DataRequired()])
-    tel2Pers        = StringField('Téléphone optionnel')
     mailPers        = StringField('Adresse Mail', validators[DataRequired()])
     clarJouees      = FieldList(FormField(StringField('Clarinette jouée')))
     niveau          = IntegerField('Année d\'experience')
+    ecole           = StringField('Ecole')
     typePratique    = StringField('Type de pratique')
 
 class RespLegalForm(FlaskForm):
-    nomResp         = StringField('Nom du Responsable')
-    prenomResp      = StringField('Prénom du Responsable')
-    adrResp         = StringField('Adresse du Responsable')
-    CPResp          = StringField('Code Postal du Responsable')
-    villeResp       = StringField('Ville du Responsable')
-    telPers         = StringField('Téléphone personnel')
+    nomResp         = StringField('Nom du Responsable', validators[DataRequired()])
+    prenomResp      = StringField('Prénom du Responsable', validators[DataRequired()])
+    adrResp         = StringField('Adresse du Responsable', validators[DataRequired()])
+    CPResp          = StringField('Code Postal du Responsable', validators[DataRequired()])
+    villeResp       = StringField('Ville du Responsable', validators[DataRequired()])
+    telPers         = StringField('Téléphone personnel', validators[DataRequired()])
     telTrav         = StringField('Téléphone travail')
-    mailPers        = StringField('Adresse Mail', validators[DataRequired()])
-    mailTrav        = StringField('Adresse Mail', validators[DataRequired()])
+    mailPers        = StringField('Adresse Mail Personnelle', validators[DataRequired()])
+    mailTrav        = StringField('Adresse Mail de Travail')
 
+class User_ContactForm(FlaskForm):
+    objetMessage    = SelectField('Objet')
+    contenuMessage  = TextField('Contenu message')
+    pjMessage       = FieldList(FormField(FileField('Pièce-jointe')))
+
+class AutorMedForm(FlaskForm):
+    numContactUrg1  = StringField('Numero à contacter Urgence 1')
+    numContactUrg2  = StringField('Numero à contacter Urgence 2')
+
+class AutorStage_MineurForm(FlaskForm):
+    autorAudioPrive = BooleanField('Autorisation utilisation audio en privé')
+    autorAudioPub   = BooleanField('Autorisation utilisation audio sur le site quatuor')
+    autorImag       = BooleanField('Autorisation utilisation image dans le cadre du stage')
+    autorImagSite   = BooleanField('Autorisation utilisation image sur le site du quatuor')
+
+class 
+
+class LoginForm(FlaskForm):
+    username        = StringField('Nom d\'utilisateur')
+    mdp             = HiddenField('Mot de passe')
 # class UserForm(FlaskForm):
 #     idUt        = HiddenField('idUt')
 #     ecoleUt     = StringField('Ecole de Musique')
