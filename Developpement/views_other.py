bcb10f7e014d2281a7f0b2187cfcf11c2b9ae6ad
Ladislas WALCAK <walcak.ladislas@gmail.com>
2019-01-18 09:25:59 +0100

    Utilisation UserMixin

--- Developpement/views_other.py
+++ Developpement/views_other.py
@@ -1,6 +1,8 @@
 from .views import *
 from .forms import *
-from flask_login import login_user
+
+from flask_login import login_user;
+
 @app.route("/other/liens/")
 def other_liens():
     """
@@ -34,7 +36,8 @@ def other_deconnexion():
 
     :return: Retourne le template de la page de deconnexion
     """
-    return render_template("other/deconnexion.html")
+    user_logout()
+    return redirect("home")
 
 @app.route("/other/mdpOubli/")
 def other_mdpOublie():
@@ -66,4 +69,3 @@ def other_test():
     form2=RespLegalForm()
     form3=AutorStage_MineurForm()
     return render_template("other/testjs.html", form=form, form2=form2, form3=form3)
-    
