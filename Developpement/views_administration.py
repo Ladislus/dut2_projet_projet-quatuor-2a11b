d889eee59bf5383920d27d4d7d8a330d4430806b
Michael Abid <abid@info23-14.iut45.univ-orleans.fr>
2019-01-18 10:56:46 +0100

    Gestion stagiaires

--- Developpement/views_administration.py
+++ Developpement/views_administration.py
@@ -1,5 +1,5 @@
 from .views import *
-@app.route("/administration")
+@app.route("/administration/")
 def administration():
     """
 
@@ -7,7 +7,7 @@ def administration():
     """
     return render_template("administration.html")
 
-@app.route("/administration/mailing")
+@app.route("/administration/mailing/")
 def administration_mailing():
     """
 
@@ -15,7 +15,7 @@ def administration_mailing():
     """
     return render_template("administration_mailing.html")
 
-@app.route("/administration/gestionStages")
+@app.route("/administration/gestionStages/")
 def administration_gestionStages():
     """
 
@@ -23,7 +23,7 @@ def administration_gestionStages():
     """
     return render_template("administration_gestionStages.html")
 
-@app.route("/administration/gestionFichiers")
+@app.route("/administration/gestionFichiers/")
 def administration_gestionFichiers():
     """
 
@@ -31,48 +31,48 @@ def administration_gestionFichiers():
     """
     return render_template("administration_gestionFichiers.html")
 
-@app.route("/administration/gestionFichiers/images")
+@app.route("/administration/gestionFichiers/images/")
 def administration_gestionFichiers_images():
     """
 
     :return: Retourne le template de la page de gestion des images
     """
-    return render_template("administration_gestionFichiers_images.html")
+    return render_template("administration/administration_gestionFichiers_images.html")
 
 
 
-@app.route("/administration/gestionFichiers/images/modification")
+@app.route("/administration/gestionFichiers/images/modification/")
 def administration_gestionFichiers_images_modification():
     """
 
     :return: Retourne le template de la page de modification d'une image qui sera dans une popup
     """
-    return render_template("administration_gestionFichiers_images_modification.html")
+    return render_template("administration/administration_gestionFichiers_images_modification.html")
 
-@app.route("/administration/gestionFichiers/videos")
+@app.route("/administration/gestionFichiers/videos/")
 def administration_gestionFichiers_videos():
     """
 
     :return: Retourne le template de la page de gestion des videos
     """
-    return render_template("administration_gestionFichiers_videos.html")
+    return render_template("administration/administration_gestionFichiers_videos.html")
 
-@app.route("/administration/gestionFichiers/videos/modification")
+@app.route("/administration/gestionFichiers/videos/modification/")
 
 def administration_gestionFichiers_videos_modification():
     """
 
     :return: Retourne le template de la page de modification d'une video qui sera dans une popup
     """
-    return render_template("administration_gestionFichiers_videos_modification.html")
+    return render_template("administration/administration_gestionFichiers_videos_modification.html")
 
-@app.route("/administration/gestionFichiers/textes")
+@app.route("/administration/gestionFichiers/textes/")
 def administration_gestionFichiers_textes():
     """
 
     :return: Retourne le template de la page de gestion des textes
     """
-    return render_template("administration_gestionFichiers_textes.html")
+    return render_template("administration/administration_gestionFichiers_textes.html")
 
 @app.route("/administration/gestionFichiers/textes/modification")
 def administration_gestionFichiers_textes_modification():
@@ -80,25 +80,32 @@ def administration_gestionFichiers_textes_modification():
 
     :return: Retourne le template de la page de modification d'un texte qui sera dans une popup
     """
-    return render_template("administration_gestionFichiers_textes_modification.html")
+    return render_template("administration/administration_gestionFichiers_textes_modification.html")
 
-@app.route("/administration/gestionStagiaires")
+@app.route("/administration/gestionStagiaires/")
 def administration_gestionStagiaires():
     """
 
     :return: Retourne le template de la page de gestion des stagiaires
     """
-    return render_template("administration_gestionStagiaires.html")
+    dico={}
+    dico['2018']=[]#Nom Prenom age niveau mail role
+    dico['2018'].append(["Abid","Michael","19","1","michael.abid@outlook.fr","user"])
+    dico['2018'].append(["Foucault","Adrien","18","2","adrien.foucault@outlook.fr","user"])
+    dico['2017']=[]#Nom Prenom age niveau mail role
+    dico['2017'].append(["Walcak","Ladislas","19","3","ladislas.walcak@outlook.fr","user"])
+    dico['2017'].append(["Demarest","Liam","19","1","liam.demarest@outlook.fr","user"])
+    return render_template("administration/administration_gestionStagiaires.html",dico_stagiaire=dico)
 
-@app.route("/administration/gestionStagiaires/Stagiaire")
+@app.route("/administration/gestionStagiaires/Stagiaire/")
 def administration_gestionStagiaires_Stagiaire():
     """
 
     :return: Retourne le template de la page de gestion d'un stagiaire
     """
-    return render_template("administration_gestionStagiaires_stagiaire.html")
+    return render_template("administration/administration_gestionStagiaires_stagiaire.html")
 
-@app.route("/administration/creerStage")
+@app.route("/administration/creerStage/")
 def administration_creerStage():
     """
 
@@ -106,6 +113,6 @@ def administration_creerStage():
     """
     creaStageForm=StageForm()
     lieuForm=LieuForm()
-    return render_template("administration/administration_creerStage.html",
+    return render_template("administration/administration/administration_creerStage.html",
                             creaStageForm=creaStageForm,
                             lieuForm=lieuForm)
