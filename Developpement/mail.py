e13da557d3093634e908f575fb4e685bd28cfdfe
LiamDem <liam.demarest@orange.fr>
2019-03-21 11:42:27 +0100

    stagiaire

--- Developpement/mail.py
+++ Developpement/mail.py
@@ -1,5 +1,18 @@
-from flask import Flask
-from flask_mail import Mail
-
-app = Flask(__name__)
-mail = Mail(app)
+# from flask import Flask
+# from flask_mail import Mail
+#
+# app = Flask(__name__)
+# mail = Mail(app)
+#
+# from flask_mail import Message
+#
+# @app.route("/send/")
+# def send():
+#
+#     msg = Message("Hello",
+#                   sender="liam.demarest@orange.fr",
+#                   recipients=["alexis.chauvette1654@gmail.com"])
+#
+#     msg.body = "testing"
+#     msg.html = "<b>testing</b>"
+#     mail.send(msg)
